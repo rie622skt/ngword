3caabac7dbd8091baa2e14f44431ecf2bf98c4aa
riE622skt <147387050+riE622skt@users.noreply.github.com>
2024-11-30 04:26:26 +0900

    del

--- app/views/room-page.ts
+++
@@ -1,7 +0,0 @@
-import { EventData, Page } from '@nativescript/core';
-import { RoomViewModel } from '../viewmodels/room-view-model';
-
-export function onNavigatingTo(args: EventData) {
-    const page = <Page>args.object;
-    page.bindingContext = new RoomViewModel();
-}
